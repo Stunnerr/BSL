923039a4e79f2dc738ef9ab90dde100f6c887b23
semgo <semgol824@gmail.com>
2020-02-10 14:40:24 +0500

    ActivityPro(java): added tab adapter and fragment, fixed deploy method

--- app/src/main/java/com/stunner/moderstars/ActivityPro.java
+++ app/src/main/java/com/stunner/moderstars/ActivityPro.java
@@ -1,10 +1,8 @@
 package com.stunner.moderstars;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -12,17 +10,20 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.stunner.moderstars.pro.Adapters.MyAdapter;
-import com.stunner.moderstars.pro.Models.TitleChild;
-import com.stunner.moderstars.pro.Models.TitleParent;
+import com.google.android.material.tabs.TabLayout;
+import com.stunner.moderstars.pro.Adapters.RecyclerViewAdapter;
+import com.stunner.moderstars.pro.Models.ListChild;
+import com.stunner.moderstars.pro.Models.ListParent;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,16 +35,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 import stunner.moderstars.R;
 
-import static com.stunner.moderstars.UsefulThings.Unzip;
-import static com.stunner.moderstars.UsefulThings.bspath;
-import static com.stunner.moderstars.UsefulThings.copy;
-import static com.stunner.moderstars.UsefulThings.sudo;
-import static com.stunner.moderstars.pro.Adapters.MyAdapter.checked;
-
+import static com.stunner.moderstars.UsefulThings.*;
 
 public class ActivityPro extends AppCompatActivity {
 int perms = 0;
@@ -65,21 +65,21 @@ int perms = 0;
         @Override
         protected Void doInBackground(Void... voids) {
             for (Object o:checked){
-                if (o.getClass().equals(TitleParent.class)){
-                    TitleParent x = (TitleParent) o;
-                    Log.d(TAG,x.getforcopy());
+                if (o.getClass().equals(ListParent.class)){
+                    ListParent x = (ListParent) o;
+                    Log.d(UsefulThings.TAG,x.getforcopy());
                     try{sudo("mkdir -p "+(bspath+"dkoskd"+ x.getforcopy()));}
-                    catch(IOException e ) {Snackbar.make(findViewById(R.id.recyclerView), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);}
+                    catch(IOException e ) {Snackbar.make(findViewById(R.id.pro_root), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);}
                 }
 
                 else
-                if (o.getClass().equals(TitleChild.class)){
-                    TitleChild x = (TitleChild) o;
+                if (o.getClass().equals(ListChild.class)){
+                    ListChild x = (ListChild) o;
                     x.getPath().mkdirs();
-                    Log.d(TAG,bspath + "dkoskd" + x.getforcopy());
+                    Log.d(UsefulThings.TAG,bspath + "dkoskd" + x.getforcopy());
                     try{copy(x.getPath(), new File(bspath + "dkoskd" + x.getforcopy()));}
                     catch (IOException e) {
-                        Snackbar.make(findViewById(R.id.recyclerView), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);
+                        Snackbar.make(findViewById(R.id.pro_root), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);
                     }
                 }
 
@@ -95,10 +95,9 @@ int perms = 0;
         }
     }
 
+    private TabsAdapter mTabsAdapter;
+    private TabLayout mTabLayout;
     FloatingActionButton fab;
-    RecyclerView recyclerView;
-    public static String TAG = "Brawl Mods";
-    //public String modspath = getExternalFilesDir("")+"/Mods/test";
     public static Context ctx;
 
     private void requestAppPermissions() {
@@ -131,16 +130,14 @@ int perms = 0;
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch(requestCode){
             case 1:
-                if(granted(grantResults))
-                {}
-                else {if(perms<5){requestAppPermissions(); perms++;}
+                if(!granted(grantResults))
+                {if(perms<5){requestAppPermissions(); perms++;}
                 else System.exit(1);}
                 break;
             case 2:
 
-                if(granted(grantResults))
-                {}
-                else {if(perms<5){requestAppPermissions(); perms++;}
+                if(!granted(grantResults))
+                {if(perms<5){requestAppPermissions(); perms++;}
                 else System.exit(1);}
                 break;
             default:
@@ -152,27 +149,29 @@ int perms = 0;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_pro, menu);
+        getMenuInflater().inflate(R.menu.menu_alpha, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_about_pro:
+            case R.id.action_about:
                 Snackbar.make(findViewById(R.id.pro_root), "Some text", Snackbar.LENGTH_SHORT).show();
 
 
                 break;
 
-            case R.id.action_settings_pro:
+            case R.id.action_settings:
                 startActivity(new Intent(this,ActivityAbout.class));
                 break;
 
-            case R.id.action_add_pro:
+            case R.id.action_add:
 
                 Choosezip();
+                break;
+            case R.id.action_alpha:
+                startActivity(new Intent(getApplicationContext(),ActivityEasy.class));
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -188,7 +187,7 @@ int perms = 0;
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.app_name);
         }
-        toolbar.inflateMenu(R.menu.menu_activity_pro);
+        toolbar.inflateMenu(R.menu.menu_release);
         bspath = getFilesDir().getAbsolutePath().split(getPackageName())[0] +"com.supercell.brawlstars/";
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -197,23 +196,20 @@ int perms = 0;
                 Choosezip();
             }
         });
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        if (initData() != null) {
+        mTabsAdapter = new TabsAdapter(getSupportFragmentManager());
+        ViewPager mViewPager = findViewById(R.id.viewpager);
+        mTabLayout = findViewById(R.id.tabs);
+        mViewPager.setAdapter(mTabsAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+        if (mTabsAdapter.getCount()>=1) {
             fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_check));
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //
                     deploy();
                 }
             });
-            MyAdapter adapter = new MyAdapter(this, initData());
-            adapter.setParentClickableViewAnimationDefaultDuration();
-            adapter.setParentAndIconExpandOnClick(true);
-            recyclerView.setAdapter(adapter);
         }
-
     }
 
 
@@ -226,56 +222,32 @@ int perms = 0;
     }
     private void deploy() {
         for (Object o:checked){
-            if (o.getClass().equals(TitleParent.class)){
-                TitleParent x = (TitleParent) o;
-                //Log.d(TAG,x.getforcopy());
-                try{sudo("mkdir -p "+(bspath+"update"+ x.getforcopy()));}
-                catch(IOException e ) {Snackbar.make(findViewById(R.id.recyclerView), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);}
+            if (o.getClass().equals(ListParent.class)){
+                ListParent x = (ListParent) o;
+                try{ sudo("mkdir -p "+(bspath+"update"+ x.getforcopy()));}
+                catch(IOException e ) {
+                    Snackbar.make(findViewById(R.id.pro_root), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);
+                }
             }
 
             else
-            if (o.getClass().equals(TitleChild.class)){
-                TitleChild x = (TitleChild) o;
+            if (o.getClass().equals(ListChild.class)){
+                ListChild x = (ListChild) o;
                 x.getPath().mkdirs();
-                //Log.d(TAG,bspath + "dkoskd" + x.getforcopy());
-                try{copy(x.getPath(), new File(bspath + "update" + x.getforcopy()));}
+                try{
+                    sudo("mkdir -p "+(bspath+"update"+ x.getforcopy().replace("/"+x.getOption1(), "/") ));
+                    copy(x.getPath(), new File(bspath + "update" + x.getforcopy()));
+
+                }
                 catch (IOException e) {
-                    Snackbar.make(findViewById(R.id.recyclerView), e.getLocalizedMessage(), Snackbar.LENGTH_LONG);
+                    Snackbar.make(findViewById(R.id.pro_root), e.getLocalizedMessage(), Snackbar.LENGTH_LONG
+                    );
                 }
             }
 
 
         }
 
-    }
-
-    public List<TitleParent> initParents(){
-        List<TitleParent> parents = new ArrayList<>();
-        for (File e:UsefulThings.checkmods(this)) parents.add(new TitleParent(e));
-        return parents;
-    }
-    private List<ParentObject> initData() {
-        int modc;
-        List<ParentObject> parentObject = new ArrayList<>();
-        modc = UsefulThings.checkmods(this).length-1;
-        if (modc != -1) {
-
-                List<TitleParent> parents = initParents();
-                for (int i = 0; i < modc; i++) {
-                    List<Object> childList = new ArrayList<>();
-                    if (UsefulThings.filelist(this, UsefulThings.checkmods(this)[i]) != null){
-                        for (File file : UsefulThings.filelist(this, UsefulThings.checkmods(this)[i])) {//список файлов
-                                childList.add(new TitleChild(file));
-
-                        }
-                    }
-                    if(childList.isEmpty()) childList.add("Это папка");
-                    parents.get(i).setChildObjectList(childList);
-                    parentObject.add(parents.get(i));
-
-                }
-            return parentObject;
-        } else return null;
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -287,20 +259,15 @@ int perms = 0;
                     String path = Environment.getExternalStorageDirectory().getPath() +'/'+ uri.toString().replaceAll("%2F", "/").split(Environment.getExternalStorageDirectory().getPath())[uri.toString().split(Environment.getExternalStorageDirectory().getPath()).length - 1];
                     Log.d(TAG, "File Path: " + path);
                     Unzip(path);
-                    if (initData() !=null){
-                    MyAdapter adapter = new MyAdapter(this, initData());
-                    adapter.setParentClickableViewAnimationDefaultDuration();
-                    adapter.setParentAndIconExpandOnClick(true);
-                    recyclerView.setAdapter(adapter);
+                    mTabLayout.addTab(mTabLayout.newTab());
+                    if (mTabsAdapter.getCount()>=1){
                         fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_check));
                         fab.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                //
                                 deploy();
-                                Snackbar.make(findViewById(R.id.recyclerView
+                                Snackbar.make(findViewById(R.id.pro_root
                                 ), R.string.success, Snackbar.LENGTH_SHORT).setDuration(900).show();
-                               // Log.d(TAG,getFilesDir().getPath() + "/com.supercell.brawlstars");
 
                             }
                         });
@@ -317,6 +284,7 @@ int perms = 0;
         intent.addCategory(Intent.CATEGORY_OPENABLE);
 
         try {
+
             startActivityForResult(
                     Intent.createChooser(intent, "Выберите архив мода"),
                     13);
@@ -325,4 +293,88 @@ int perms = 0;
                     Toast.LENGTH_SHORT).show();
         }
     }
+    public class TabsAdapter extends FragmentPagerAdapter {
+
+        TabsAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return TabsFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return UsefulThings.modcount(getApplicationContext());
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Mod #"+ (position+1);
+        }
+    }
+    public static class TabsFragment extends Fragment {
+        private int mPage;
+        private static final String ARG_PAGE = "section_number";
+
+        public TabsFragment() {
+        }
+        static ActivityEasy.TabsFragment newInstance(int sectionNumber) {
+            ActivityEasy.TabsFragment fragment = new ActivityEasy.TabsFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_PAGE, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+        RecyclerViewAdapter adapter;
+        @Override public void onCreate(Bundle savedInstanceState) {
+            if (getArguments() != null) {
+                mPage = getArguments().getInt(ARG_PAGE);
+            }
+            adapter = new RecyclerViewAdapter(getContext(), initData(mPage));
+            adapter.setParentClickableViewAnimationDefaultDuration();
+            adapter.setParentAndIconExpandOnClick(true);
+            super.onCreate(savedInstanceState);
+        }
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_pro, container, false);
+            RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(adapter);
+            return rootView;
+
+        }
+        List<ListParent> initParents(int modc){
+            List<ListParent> parents = new ArrayList<>();
+            for (File e:UsefulThings.checkmod(getContext(), modc)) parents.add(new ListParent(e, modc));
+            return parents;
+        }
+        private List<ParentObject> initData(int modc) {
+            int folc;
+            List<ParentObject> parentObject = new ArrayList<>();
+            folc = UsefulThings.checkmod(getContext(), modc).length-1;
+            if (folc != -1) {
+
+                List<ListParent> parents = initParents(modc);
+                for (int i = 0; i < folc; i++) {
+                    List<Object> childList = new ArrayList<>();
+                    if (UsefulThings.filelist(getContext(), UsefulThings.checkmod(getContext(), modc)[i]) != null){
+                        for (File file : UsefulThings.filelist(getContext(), UsefulThings.checkmod(getContext(), modc)[i])) {
+                            childList.add(new ListChild(file, modc));
+                        }
+                    }
+                    if (childList.isEmpty()) childList.add("Это файл");
+                    parents.get(i).setChildObjectList(childList);
+                    parentObject.add(parents.get(i));
+
+                }
+                return parentObject;
+            } else return null;
+        }
+    }
+
 }
